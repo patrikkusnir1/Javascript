be996bad4532eb4672569d7021f2b76eaae3bd0f
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-05-21 14:24:54 +0200

    unitialized variables

--- youtube_fcc/app.js
+++ youtube_fcc/app.js
@@ -1,2 +1,16 @@
-// initializing variables with assignment operator
-var a = 9; 
+// unitialized variables
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+a = a + 1;
+b = b + 5;
+c = c + " String!"
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
