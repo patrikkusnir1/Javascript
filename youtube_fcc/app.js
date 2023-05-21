e9368dc3ef9da9583e846e79296c7012952610b2
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-05-21 14:09:15 +0200

    // initializing variables with assignment operator

--- youtube_fcc/app.js
+++ youtube_fcc/app.js
@@ -1,11 +1,2 @@
-// Storing values with assignment operator
-var a;
-var b = 2;
-console.log(a);
-a = 7;
-
-b = a;
-
-console.log(a)
-
-// it shows first undefined/null for a and then 7
+// initializing variables with assignment operator
+var a = 9; 
