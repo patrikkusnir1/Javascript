7c7ffb9d2639c5c580d83fae775f9a2ab51a595c
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-05-23 17:50:53 +0200

    // Compound Assignment With Augmented Division

--- youtube_fcc/app.js
+++ youtube_fcc/app.js
@@ -1,10 +1,10 @@
-// Compound Assignment With Augmented Multiplication
-var a = 5;
-var b = 12;
-var c = 4.6;
+// Compound Assignment With Augmented Division
+var a = 48;
+var b = 108;
+var c = 33;
 
-a *= 5;
-b *= 3;
-c *= 10;
+a /= 12;
+b /= 4; 
+c /= 11;
 
 console.log(a, b, c);
