0d00cdb3666e6bec5d358e59ed46d3ada4b66d81
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-05-23 17:59:49 +0200

    Escaping Literal Quotes in Strings

--- youtube_fcc/app.js
+++ youtube_fcc/app.js
@@ -1,8 +1,3 @@
-// Declare string variables
-
-// Example
-var firstName = "Alan";
-var lastName = "Turing";
-
-var myFirstName = "Beau";
-var myLastName = "Carnes"
+// Escaping Literal Quotes in Strings
+var myStr = "I am a \"double quoted\" string inside \"double quotes\""
+console.log(myStr)
