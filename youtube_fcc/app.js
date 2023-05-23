efd0f075af5cf26ef621aa34ec7236cb8ac40592
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-05-23 17:47:02 +0200

    Compound Assignment With Augmented Multiplication

--- youtube_fcc/app.js
+++ youtube_fcc/app.js
@@ -1,10 +1,10 @@
-// Compound Assignment With Augmented Subtraction
-var a = 11;
-var b = 9;
-var c = 3;
+// Compound Assignment With Augmented Multiplication
+var a = 5;
+var b = 12;
+var c = 4.6;
 
-a -= 6;
-b -= 15;
-c -= 1;
+a *= 5;
+b *= 3;
+c *= 10;
 
 console.log(a, b, c);
