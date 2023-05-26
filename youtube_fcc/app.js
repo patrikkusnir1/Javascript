f317cb3fb8a0cc075c010b835aa6d3b3a6e6ce83
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-05-26 10:20:14 +0200

    Manipulate Arrays With unshift Method

--- youtube_fcc/app.js
+++ youtube_fcc/app.js
@@ -1,10 +1,12 @@
-// Manipulate Arrays With shift Method
+// Manipulate Arrays With unshift Method
 
 // Example
 var ourArray = ["Stimpson", "J", ["cat"]];
-var removedFromOurArray = ourArray.shift();
+ourArray.shift();
+ourArray.unshift("Happy");
 
 // Setup 
-var myArray = [["John", 23],["dog", 3]]
-var removedFromMyArray = myArray.shift();
-console.log(removedFromMyArray);
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
+console.log(myArray);
