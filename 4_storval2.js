96589cef368bb77fb5c89d20bf70aed9994181d7
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2023-09-03 12:45:56 +0300

    Add files via upload

---
+++ 4_storval2.js
@@ -0,0 +1,14 @@
+//Assigning the Value of One Variable to Another
+
+var myVar;
+myVar = 5;
+var myNum;
+myNum = myVar;
+
+// Assign the contents of a to variable b.
+
+// Setup 
+var a;
+a = 7;
+var b;
+b = a;
