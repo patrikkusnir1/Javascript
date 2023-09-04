d302ff55736346b1805fc5fbaed9998bcf29ac8e
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2023-09-04 21:45:48 +0300

    Add files via upload

---
+++ 21_comp_assign.js
@@ -0,0 +1,10 @@
+// Compound Assignment With Augmented Addition
+
+// Convert the assignments for a, b, and c to use the += operator.
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
