3abb457b03986c5954f79c510cedc8c52f991ecd
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2023-09-05 08:23:09 +0300

    Add files via upload

---
+++ 26_escape_strings.js
@@ -0,0 +1,11 @@
+// Quoting Strings with Single Quotes
+
+const doubleQuoteStr = "This is a string";
+const singleQuoteStr = 'This is also a string';
+const conversation = 'Finn exclaims to Jake "Algebraic!"';
+const goodStr = 'Jake asks Finn, "Hey, let\'s go on adventure?"';
+// const badStr = 'Finn responds, "Let's go!"' throws error
+
+const myStr = "<a href=\"http://www.example.com\"target=\"_blank\">Link</a>";
+// Right now, the <a> tag in the string uses double quotes everywhere. You will need to change the outer quotes to single quotes so you can remove the escape characters.
+const myStr2 = '<a href="http://www.example.com"target="_blank">Link</a>'
---
+++ 28_concanet_strings.js
@@ -0,0 +1,6 @@
+// Concatenating Strings with Plus Operator
+
+let str = 'My name is Alan' + ' I concanate';
+const ourStr = "I come first. " + "I come second."
+console.log(ourStr)
+const myStr = "This is the start." + " This is the end."
